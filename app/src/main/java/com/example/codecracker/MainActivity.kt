3dfab48703957b17585433ccab114ec69fcf4e06
nagaham <nagahama2015@gmail.com>
2022-04-15 15:18:02 +0900

    add spinner

--- app/src/main/java/com/example/codecracker/MainActivity.kt
+++ app/src/main/java/com/example/codecracker/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.codecracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import com.example.codecracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        binding.spinner01.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val spinner = parent as? Spinner
+                val item = spinner?.selectedItem as? String
+                item?.let {
+                    if (it.isNotEmpty()) binding.textView01.text = it
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        binding.spinner02.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val spinner = parent as? Spinner
+                val item = spinner?.selectedItem as? String
+                item?.let {
+                    if (it.isNotEmpty()) binding.textView02.text = it
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        binding.spinner03.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val spinner = parent as? Spinner
+                val item = spinner?.selectedItem as? String
+                item?.let {
+                    if (it.isNotEmpty()) binding.textView03.text = it
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        binding.spinner04.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val spinner = parent as? Spinner
+                val item = spinner?.selectedItem as? String
+                item?.let {
+                    if (it.isNotEmpty()) binding.textView04.text = it
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
 
     }
 }
