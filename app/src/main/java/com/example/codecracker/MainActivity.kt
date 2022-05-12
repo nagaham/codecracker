a5cd13a9d508be0ab131883f4af233358bb90dfd
nagaham <nagahama2015@gmail.com>
2022-05-12 15:45:40 +0900

    improve restart

--- app/src/main/java/com/example/codecracker/MainActivity.kt
+++ app/src/main/java/com/example/codecracker/MainActivity.kt
@@ -18,8 +18,8 @@ var perfectnum = 0 //correct の数が何個あるか
 var closenum = 0 //文字列の中で正解の数字が何個あるか
 var numoftrials = 1 //挑戦回数をカウントする変数
 var outputtext = ""//出力用文字列
-var inputnumber = ""
-var historynumber = ""
+var inputnumber = "" //入力した文字列を収納する変数
+var historynumber = "" //入力履歴と判定を残す変数
 
 //初期設定で自分の答えを ____ にする
 var yourans = mutableListOf<String>("_","_","_","_")
@@ -38,58 +38,68 @@ class MainActivity : AppCompatActivity() {
 
         binding.button01.setOnClickListener {
 
-            if(binding.button01.text == "Restart"){
-                initial()
-            }
+            if(binding.button01.text == "Restart") initial()//初期化して乱数も取り直す
 
-            inputnumber = binding.inputnum01.text.toString()
-            historynumber += inputnumber
-            outputtext = ""//出力用文字列を初期化
+            else {
 
-        //正解するまで繰り返す
-            if(inputnumber.length < 4 ){
-                inputnumber = "____"
-                outputtext = "4文字未満で判定できず\n"
-                binding.inputnum01.text = null
-            }
-            else if(inputnumber.length > 4){
-                inputnumber = "____"
-                outputtext = "4文字以上で判定できず\n"
-                binding.inputnum01.text = null
-            }
-            else{
+                inputnumber = binding.inputnum01.text.toString()
+                historynumber += inputnumber
                 outputtext = ""
-            }
-            outputtext += "⭐️$numoftrials 回目\n"
-            numoftrials++
 
-            countOfNumCorrectClose() //correctとcloseの数字がそれぞれ何個あるのかをカウント
+                //正解するまで繰り返す
 
-            for(n in 0 .. 9){
-                if(min(countofyournumber[n],countofcorrectans[n]) != 0){
-                    outputtext += "$n を発見\n"
+                judgeNumOfStrings()
+
+                outputtext += "⭐️$numoftrials 回目\n"
+                numoftrials++
+
+                countOfNumCorrectClose() //correctとcloseの数字がそれぞれ何個あるのかをカウント
+
+                for (n in 0..9) {
+                    if (min(countofyournumber[n], countofcorrectans[n]) != 0) {
+                        outputtext += "$n を発見\n"
+                    }
+                    closenum += min(
+                        countofyournumber[n],
+                        countofcorrectans[n]
+                    )//closenum にはこの時点で場所は関係なく数字が合っている個数が入る
                 }
-                closenum += min(countofyournumber[n], countofcorrectans[n])//closenum にはこの時点で場所は関係なく数字が合っている個数が入る
-            }
 
-            for (i in 0..3) {
-                if (inputnumber[i] == randnumber[i]) {
-                    yourans[i] = randnumber[i].toString()
-                    perfectnum++
+                for (i in 0..3) {
+                    if (inputnumber[i] == randnumber[i]) {
+                        yourans[i] = randnumber[i].toString()
+                        perfectnum++
+                    }
                 }
+
+                closenum -= perfectnum //closenum に　場所は合っていないが数字が合っている個数を入れる
+
+                outputtext += "完璧 $perfectnum 個\n場所違い$closenum 個\n"
+                historynumber += " Correct " + perfectnum + "  Close " + closenum + "\n"
+                binding.textView01.text = outputtext
+                binding.history01.text = historynumber
+
+                congratulations()
             }
-
-            closenum -= perfectnum //closenum に　場所は合っていないが数字が合っている個数を入れる
-
-            outputtext += "完璧 $perfectnum 個\n場所違い$closenum 個\n"
-            historynumber += " Correct " + perfectnum + "  Close " + closenum + "\n"
-            binding.textView01.text = outputtext
-            binding.history01.text = historynumber
-
-            congratulations()
         }
     }
 
+    // 入力した文字数が正しいかどうかを判断する
+    fun judgeNumOfStrings(){
+        if(inputnumber.length < 4 ){
+            inputnumber = "____"
+            outputtext = "4文字未満で判定できず\n"
+            binding.inputnum01.text = null
+        }
+        else if(inputnumber.length > 4){
+            inputnumber = "____"
+            outputtext = "4文字以上で判定できず\n"
+            binding.inputnum01.text = null
+        }
+        else{
+            outputtext = ""
+        }
+    }
     // それぞれの変数を初期化
     fun initial(){
         countofcorrectans = listOf<Int>(0,0,0,0,0,0,0,0,0,0) as MutableList<Int> //0~9までの数字について正解を収納
@@ -101,7 +111,12 @@ class MainActivity : AppCompatActivity() {
         inputnumber = ""
         historynumber = ""
 
-//初期設定で自分の答えを ____ にする
+        binding.textViewfin.text = null //完璧に正解したときの絵文字を初期化
+        binding.history01.text = null //履歴の表示欄をリセット
+        binding.inputnum01.text = null //入力した文字列を消去
+        binding.textView01.text = null //判定時に出るコメントをリセット
+
+        //初期設定で自分の答えを ____ にする
         yourans = mutableListOf<String>("_","_","_","_")
         randnumber = ""
         determineRand()
@@ -129,7 +144,7 @@ class MainActivity : AppCompatActivity() {
             outputtext += "Great!!!\n" + "${numoftrials - 1} 回目で成功！\n"
             binding.textView01.text = outputtext
             outputtext = ""
-            binding.textViewfin.text = "😎\n🏆"
+            binding.textViewfin.text = "😎\n🎉"
 
             binding.button01.text = "Restart"
         }
